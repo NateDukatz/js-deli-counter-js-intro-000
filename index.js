034c01d8505ea908962d309bf6f7b57384e8e6ff
Nate Dukatz <natedukatz@gmail.com>
2018-07-16 06:25:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function takeANumber(katzDeliLine, name) {
+  for (var i = 0; i <= katzDeliLine.length; i++) {
+    if (katzDeliLine[i] = name) {
+      return `Welcome, Ada. You are number ${i + 1} in line.`
+    }
+  }
+}
